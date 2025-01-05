0101ddabc95af8f3cefd35548edc5606586b1db3
ElonaColi <elonnaacoli@gmail.com>
2025-01-05 23:34:19 +0100

    validation

--- validation.js
+++ validation.js
@@ -1,28 +1,33 @@
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-function validateForm(){
+   if (validateForm()){
+    alert("Form submitted successfully!");
+
+   }
+});
+function validateForm() {
     var username= document.getElementById("username").value;
     var email=document.getElementById("email").value;
     var password=document.getElementById("password").value;
-    var password=document.getElementById("conirmPassword").value;
-    
-
-
-    if(username=="" || email=="" || password==""){
-        alert("All fields are required");
-        return false;
-    }
-    if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+    var conirmPassword=document.getElementById("conirmPassword").value;
+    const emailRegex=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(!emailRegex.test(email)){
         alert("Invalid email address!");
         return false;
 
     }
     const passwordRedex=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    if(!passwordRedex(password)){
+    if(!passwordRedex.test(password)){
         alert("Password must be at least 8 characters long, and include uppercase, lowercase, a number, and a special character.");
+        return false;
+    }
+    if(password !== conirmPassword){
+        alert("Passwords do not match.");
         return false;
     }
     return true;
 }
-});
+
+   
+   
