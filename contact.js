b8bd863875ca659756c9c217084d6cd4b7e76255
ErdaMusolli <erdamusolli@gmail.com>
2025-01-11 23:00:13 +0100

    front validation

---
+++ contact.js
@@ -0,0 +1,5 @@
+function validateForm(){
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message =document.getElementById('message').value;
+}
