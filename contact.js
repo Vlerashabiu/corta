1a586212f11ca104b09b41a967fb73564308272f
ErdaMusolli <erdamusolli@gmail.com>
2025-01-11 23:55:55 +0100

    contact validation

--- contact.js
+++ contact.js
@@ -1,5 +1,28 @@
-function validateForm(){
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log("Form submitted");
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    const message =document.getElementById('message').value;
-}
+    const message = document.getElementById('message').value;
+
+   
+    if(name === "" || email === "" || message === ""){
+        alert("All fields are required!");
+        return false;
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if(!emailRegex.test(email)){
+         alert("Invalid email address!");
+        return false;
+    }
+
+    if(message.length < 20) {
+        alert("The message must be at least 20 characters long.");
+        return false;
+    }
+
+    alert("Your message has been successfully sent!");
+    return true;
+
+});
