590ab3c84aa5921252bcf75bfc6cae8e362f0a88
ElonaColi <elonnaacoli@gmail.com>
2025-01-18 22:38:00 +0100

    validimi

--- forgotpasswordvalidation.js
+++ forgotpasswordvalidation.js
@@ -1,24 +1,22 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    const form = document.getElementById('signupForm');
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('form');
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-       if (validateForm()){
-        alert("Form submitted successfully!");
-    
-       }
+        if (validateForm()) {
+            alert("Form submitted successfully!");
+        }
     });
+
     function validateForm() {
+        var email = document.getElementById('email').value;
+        const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
         
-        var email=document.getElementById('email').value;
-        
-        const emailRegex=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        if(!emailRegex.test(email)){
+        if (!emailRegex.test(email)) {
             alert("Invalid email address!");
             return false;
-    
         }
         
         return true;
     }
-    });
+});
     
