f98dc29ff03763a75cc4e71deed1964f0130acef
ElonaColi <elonnaacoli@gmail.com>
2025-01-29 21:30:29 +0100

    changes

--- loginvalidation.js
+++ loginvalidation.js
@@ -1,29 +1,28 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', ()=> {
     const form = document.getElementById('form');
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-       if (validateForm()){
-        alert("Form submitted successfully!");
-    
-       }
+        if (validateForm()) {
+            alert("Form submitted successfully!");
+        }
     });
+
     function validateForm() {
-        
-        var email=document.getElementById('email').value;
-        var password=document.getElementById('password').value;
-        
-        const emailRegex=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        if(!emailRegex.test(email)){
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+
+        const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (!emailRegex.test(email)) {
             alert("Invalid email address!");
             return false;
-    
         }
-        const passwordRedex=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-        if(!passwordRegex.test(password)){
+
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        if (!passwordRegex.test(password)) {
             alert("Password must be at least 8 characters long, and include uppercase, lowercase, a number, and a special character.");
             return false;
         }
-        
+
         return true;
     }
-    });
+});
