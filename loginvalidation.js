d07554363ea1f3944ca3bb674591b886b8ba4238
ElonaColi <elonnaacoli@gmail.com>
2025-01-29 14:00:46 +0100

    login validation

--- loginvalidation.js
+++ loginvalidation.js
@@ -8,7 +8,7 @@ document.getElementById('form').addEventListener('submit', function(event){
         alert('Invalid email address!');
         return false;
     }
-    const passwordRegex=/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const passwordRedex=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     if(!passwordRegex.test(password)){
         alert('Password must be at least 8 characters long, and include uppercase, lowercase, a number, and a special character.');
         return false;
