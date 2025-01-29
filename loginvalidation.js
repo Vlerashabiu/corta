58357a1fbd1eb44234e469907f5ed36e42d8d13d
ElonaColi <elonnaacoli@gmail.com>
2025-01-29 13:56:28 +0100

    validmi login

--- loginvalidation.js
+++ loginvalidation.js
@@ -1,13 +1,14 @@
 document.getElementById('form').addEventListener('submit', function(event){
     event.preventDefault();
     var email=document.getElementById('email').value; 
-    const emailRegex=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     var password=document.getElementById('password').value;
-    const passwordRegex=/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+   
+    const emailRegex=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if(!emailRegex.test(email)){
         alert('Invalid email address!');
         return false;
     }
+    const passwordRegex=/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     if(!passwordRegex.test(password)){
         alert('Password must be at least 8 characters long, and include uppercase, lowercase, a number, and a special character.');
         return false;
