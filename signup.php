38d963816acab9ae6f7b8fd713b5e2f3b50aab31
ErdaMusolli <erdamusolli@gmail.com>
2025-01-29 23:27:27 +0100

    signup changes

--- signup.php
+++ signup.php
@@ -1,9 +1,9 @@
 <?php
-// Connection to the database
+
 $servername = "localhost";
-$username = "root"; // your db username
-$password = ""; // your db password
-$dbname = "corta"; // your database name
+$username = "root";
+$password = ""; 
+$dbname = "corta"; 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -11,21 +11,21 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    echo "Forma u dërgua!"; // Kjo do të shfaqet nëse formulari është dërguar me POST
-    // Shiko të dhënat e dërguara
+    echo "Forma u dërgua!";
+  
     var_dump($_POST);
 }
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $email = $_POST['email'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash the password for security
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); 
     $role = $_POST['role'];
 
     $sql = "INSERT INTO users (username, email, password, role) VALUES ('$username', '$email', '$password', '$role')";
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
-        header("Location: login.html"); // Redirect to login page after successful signup
+        header("Location: login.html"); 
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
