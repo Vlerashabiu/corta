d8494539f72d538622e8b8b418ce13c0b3723b35
ErdaMusolli <erdamusolli@gmail.com>
2025-01-29 01:29:32 +0100

    index

--- login.php
+++ login.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if ($user['role'] === 'admin') {
                 header("Location: admin_dashboard.php");
             } else {
-                header("Location: user_dashboard.php");
+                header("Location: index.php");
             }
             exit();
         } else {
