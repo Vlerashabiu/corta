cbbf4cb5fc7335997ac54431d12e76e13738ae9c
Vlera <vs70105@ubt-uni.net>
2025-01-30 21:39:10 +0100

    store.php

--- store.php
+++ store.php
@@ -1,69 +1,55 @@
 <?php
-$servername = "localhost";
-$username = "root"; 
-$password = ""; 
-$dbname = "corta";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-
-if ($conn->connect_error) {
-    die("Lidhja dështoi: " . $conn->connect_error);
-}
-
-
-$sql = "SELECT * FROM products";
-$result = $conn->query($sql);
+$products = [
+    ["link" => "image1.html", "image" => "foto1.png", "name" => "Chic Corduroy Bag", "price" => "$65.00"],
+    ["link" => "image2.html", "image" => "foto2.png", "name" => "Urban Denim", "price" => "$68.00"],
+    ["link" => "image3.html", "image" => "foto3.png", "name" => "Ocean Breeze Linen", "price" => "$76.00"],
+    ["link" => "image4.html", "image" => "foto4.png", "name" => "Canvas Coast", "price" => "$60.00"],
+    ["link" => "image5.html", "image" => "foto5.png", "name" => "Eco Cedar Bag", "price" => "$65.00"],
+    ["link" => "image6.html", "image" => "foto6.png", "name" => "Kyoto Bamboo Bag", "price" => "$65.00"],
+    ["link" => "image7.html", "image" => "foto7.png", "name" => "Denim Wanderer", "price" => "$48.00"],
+    ["link" => "image8.html", "image" => "foto8.png", "name" => "Bamboo Temple", "price" => "$50.00"],
+    ["link" => "image9.html", "image" => "foto9.png", "name" => "Seaside Linen Bag", "price" => "$70.00"]
+];
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Store - CORTA</title>
+    <title>Welcome to CORTA</title>
     <link rel="stylesheet" href="store.css">
+    <link href="https://fonts.googleapis.com/css2?family=Major+Mono+Display&display=swap" rel="stylesheet">
 </head>
 <body>
-
 <header class="navbar">
     <div class="title">CORTA</div>
     <a href="home.html">Home</a>
-    <a href="store.php">Store</a>
+    <a href="store.html">Store</a>
     <a href="contact.html">ContactUs</a>
     <a href="news.html">News</a>
-    <button class="sign-up" onclick="window.location.href='signup.html'">Sign up</button>
-    <button class="log-in" onclick="window.location.href='login.html'">Log in</button>
+    <button class="sign-up"><a href="signup.html">Sign up</a></button> 
+    <button class="log-in"><a href="login.html">Log in</a></button>
 </header>
-
 <h1>The Boutique</h1>
-
 <div class="image">
-    <?php
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            echo '<div class="product">';
-            echo '<a href="product.php?id=' . $row['id'] . '">';
-            echo '<img src="' . $row['image'] . '" alt="' . $row['name'] . '">';
-            echo '</a>';
-            echo '<h4>' . $row['name'] . '</h4>';
-            echo '<p>$' . number_format($row['price'], 2) . '</p>';
-            echo '</div>';
-        }
-    } else {
-        echo "<p>No products available.</p>";
-    }
-    ?>
+    <?php foreach (array_chunk($products, 3) as $row): ?>
+        <div class="image-row">
+            <?php foreach ($row as $product): ?>
+                <div class="image-item">
+                    <a href="<?= $product['link']; ?>">
+                        <img src="<?= $product['image']; ?>" alt="<?= $product['name']; ?>">
+                    </a>
+                    <h4><?= $product['name']; ?></h4>
+                    <p><?= $product['price']; ?></p>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php endforeach; ?>
 </div>
-
 <footer class="site-footer">
-    <p>Copyright © 2024 - 2025 Corta, All Rights Reserved.</p>
+    <div class="footer">
+        <p>Copyright © 2024 - 2025 Corta, All Rights Reserved.</p>
+    </div>
 </footer>
-
 </body>
 </html>
-
-<?php
-$conn->close();
-?>
-
