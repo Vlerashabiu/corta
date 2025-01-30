e348bc4d46e84a5fe43c5ae455c94e77b222117e
Vlera <vs70105@ubt-uni.net>
2025-01-30 21:54:05 +0100

    store.php changes

--- store.php
+++ store.php
@@ -1,22 +1,85 @@
 <?php
+class Product {
+    private $link;
+    private $image;
+    private $name;
+    private $price;
+
+    // Konstruktor
+    public function __construct($link, $image, $name, $price) {
+        $this->setLink($link);
+        $this->setImage($image);
+        $this->setName($name);
+        $this->setPrice($price);
+    }
+
+    // Setters
+    public function setLink($link) {
+        if (empty($link)) {
+            throw new Exception("Linku nuk mund të jetë i zbrazët");
+        }
+        $this->link = $link;
+    }
+
+    public function setImage($image) {
+        if (empty($image)) {
+            throw new Exception("Imazhi nuk mund të jetë i zbrazët");
+        }
+        $this->image = $image;
+    }
+
+    public function setName($name) {
+        if (empty($name)) {
+            throw new Exception("Emri i produktit nuk mund të jetë i zbrazët");
+        }
+        $this->name = $name;
+    }
+
+    public function setPrice($price) {
+        if (empty($price)) {
+            throw new Exception("Çmimi nuk mund të jetë i zbrazët");
+        }
+        $this->price = $price;
+    }
+
+    // Getters
+    public function getLink() {
+        return $this->link;
+    }
+
+    public function getImage() {
+        return $this->image;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+}
+
 $products = [
-    ["link" => "image1.html", "image" => "foto1.png", "name" => "Chic Corduroy Bag", "price" => "$65.00"],
-    ["link" => "image2.html", "image" => "foto2.png", "name" => "Urban Denim", "price" => "$68.00"],
-    ["link" => "image3.html", "image" => "foto3.png", "name" => "Ocean Breeze Linen", "price" => "$76.00"],
-    ["link" => "image4.html", "image" => "foto4.png", "name" => "Canvas Coast", "price" => "$60.00"],
-    ["link" => "image5.html", "image" => "foto5.png", "name" => "Eco Cedar Bag", "price" => "$65.00"],
-    ["link" => "image6.html", "image" => "foto6.png", "name" => "Kyoto Bamboo Bag", "price" => "$65.00"],
-    ["link" => "image7.html", "image" => "foto7.png", "name" => "Denim Wanderer", "price" => "$48.00"],
-    ["link" => "image8.html", "image" => "foto8.png", "name" => "Bamboo Temple", "price" => "$50.00"],
-    ["link" => "image9.html", "image" => "foto9.png", "name" => "Seaside Linen Bag", "price" => "$70.00"]
+    new Product("image1.html", "foto1.png", "Chic Corduroy Bag", "$65.00"),
+    new Product("image2.html", "foto2.png", "Urban Denim", "$68.00"),
+    new Product("image3.html", "foto3.png", "Ocean Breeze Linen", "$76.00"),
+    new Product("image4.html", "foto4.png", "Canvas Coast", "$60.00"),
+    new Product("image5.html", "foto5.png", "Eco Cedar Bag", "$65.00"),
+    new Product("image6.html", "foto6.png", "Kyoto Bamboo Bag", "$65.00"),
+    new Product("image7.html", "foto7.png", "Denim Wanderer", "$48.00"),
+    new Product("image8.html", "foto8.png", "Bamboo Temple", "$50.00"),
+    new Product("image9.html", "foto9.png", "Seaside Linen Bag", "$70.00")
 ];
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to CORTA</title>
+    <!-- Lidhja me skedarin CSS -->
     <link rel="stylesheet" href="store.css">
     <link href="https://fonts.googleapis.com/css2?family=Major+Mono+Display&display=swap" rel="stylesheet">
 </head>
@@ -27,29 +90,32 @@ $products = [
     <a href="store.html">Store</a>
     <a href="contact.html">ContactUs</a>
     <a href="news.html">News</a>
-    <button class="sign-up"><a href="signup.html">Sign up</a></button> 
+    <button class="sign-up"><a href="signup.html">Sign up</a></button>
     <button class="log-in"><a href="login.html">Log in</a></button>
 </header>
+
 <h1>The Boutique</h1>
 <div class="image">
     <?php foreach (array_chunk($products, 3) as $row): ?>
         <div class="image-row">
             <?php foreach ($row as $product): ?>
                 <div class="image-item">
-                    <a href="<?= $product['link']; ?>">
-                        <img src="<?= $product['image']; ?>" alt="<?= $product['name']; ?>">
+                    <a href="<?= $product->getLink(); ?>">
+                        <img src="<?= $product->getImage(); ?>" alt="<?= $product->getName(); ?>">
                     </a>
-                    <h4><?= $product['name']; ?></h4>
-                    <p><?= $product['price']; ?></p>
+                    <h4><?= $product->getName(); ?></h4>
+                    <p><?= $product->getPrice(); ?></p>
                 </div>
             <?php endforeach; ?>
         </div>
     <?php endforeach; ?>
 </div>
+
 <footer class="site-footer">
     <div class="footer">
         <p>Copyright © 2024 - 2025 Corta, All Rights Reserved.</p>
     </div>
 </footer>
+
 </body>
 </html>
