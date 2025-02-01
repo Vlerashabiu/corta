1fff8abd614a28fb53372f1d4132f857cebf6df5
Vlera <vs70105@ubt-uni.net>
2025-02-02 00:12:31 +0100

    changes

--- image6.php
+++ image6.php
@@ -6,7 +6,7 @@ class Navbar {
         echo '
         <header class="navbar">
             <div class="title">CORTA</div>
-            <a href="home.php">Home</a>
+            <a href="index.php">Home</a>
             <a href="store.php">Store</a>
             <a href="contact.php">ContactUs</a>
             <a href="news.php">News</a>
